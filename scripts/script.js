4a76df12c2b2520c9dcf4d7e2d9aff7061fb0d0a
vitakat <70508211+vitakat@users.noreply.github.com>
2022-11-08 11:39:53 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
-console.log("Howdy!");
+
+// var deButton = document.querySelector("ul li:nth-of-type(1) img");
+// deButton.addEventListener("click", verplaatsen)
+// function verplaatsen() {
+// deButton.classList.toggle("snoopys");
+// }
+var deButton = document.querySelector("ol li:nth-of-type(1) button");
+
+deButton.addEventListener("click", veranderen);
+
+function veranderen() { 
+    deButton.classList.toggle("veranderen");
+}
+
+var deTweedeButton = document.querySelector("ol li:nth-of-type(2) button");
+deTweedeButton.addEventListener("click", veranderen2);
+function veranderen2() { 
+    deTweedeButton.classList.toggle("Geklikt");
+}
