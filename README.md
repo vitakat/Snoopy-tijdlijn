76b533c8e1693990ec5dd8f5406b31ec6fc1fb91
vitakat <70508211+vitakat@users.noreply.github.com>
2022-11-09 12:53:52 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -12,10 +12,10 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
 ## Jij
 
 ### Ontwerper:
-Sanne 't Hooft (vervangen door jouw naam)
+Vita Sznaider
 
 #### Je startniveau:
-Mijn startniveau is: hier je startniveau (kies uit zwart, rood óf blauw)
+Mijn startniveau is blauw
 
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,48 @@
 // JavaScript Document
+// var achtergrond = document.querySelector('body main > img:first-of-type')
 
-// var deButton = document.querySelector("ul li:nth-of-type(1) img");
-// deButton.addEventListener("click", verplaatsen)
-// function verplaatsen() {
-// deButton.classList.toggle("snoopys");
-// }
 var deButton = document.querySelector("ol li:nth-of-type(1) button");
-
 deButton.addEventListener("click", veranderen);
 
 function veranderen() { 
     deButton.classList.toggle("veranderen");
+    document.body.classList.toggle("detailstonen");
+
+    var snoopy1 = document.querySelector('ol li:nth-of-type(1) button img')
+
+    // achtergrond
+    document.body.classList.toggle("snoopy1");
+
 }
 
 var deTweedeButton = document.querySelector("ol li:nth-of-type(2) button");
 deTweedeButton.addEventListener("click", veranderen2);
 function veranderen2() { 
-    deTweedeButton.classList.toggle("Geklikt");
+    deTweedeButton.classList.toggle("veranderen");
+    document.body.classList.toggle("detailstonen");
+
+    // achtergrond
+    // document.body.classList.toggle("snoopy2");
+
+}
+
+var deDerdeButton = document.querySelector("ol li:nth-of-type(3) button");
+deDerdeButton.addEventListener("click", veranderen3);
+function veranderen3() { 
+    deDerdeButton.classList.toggle("veranderen");
+    document.body.classList.toggle("detailstonen");
+
+    // achtergrond
+    document.body.classList.toggle("snoopy3");
+}
+
+
+var deVierdeButton = document.querySelector("ol li:nth-of-type(4) button");
+deVierdeButton.addEventListener("click", veranderen4);
+function veranderen4() { 
+    deVierdeButton.classList.toggle("veranderen");
+    document.body.classList.toggle("detailstonen");
+
+    // achtergrond
+    document.body.classList.toggle("snoopy4");
 }
