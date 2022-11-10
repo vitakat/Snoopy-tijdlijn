0d0c773f439c40e7dea0863e785bc646f96daceb
vitakat <70508211+vitakat@users.noreply.github.com>
2022-11-10 15:07:49 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,9 @@
 var deButton = document.querySelector("ol li:nth-of-type(1) button");
 deButton.addEventListener("click", veranderen);
 
-function veranderen() { 
+function veranderen() {
     deButton.classList.toggle("veranderen");
     document.body.classList.toggle("detailstonen");
-
     var snoopy1 = document.querySelector('ol li:nth-of-type(1) button img')
 
     // achtergrond
@@ -17,18 +16,20 @@ function veranderen() {
 
 var deTweedeButton = document.querySelector("ol li:nth-of-type(2) button");
 deTweedeButton.addEventListener("click", veranderen2);
-function veranderen2() { 
+
+function veranderen2() {
     deTweedeButton.classList.toggle("veranderen");
     document.body.classList.toggle("detailstonen");
 
     // achtergrond
-    // document.body.classList.toggle("snoopy2");
+    document.body.classList.toggle("snoopy2");
 
 }
 
 var deDerdeButton = document.querySelector("ol li:nth-of-type(3) button");
 deDerdeButton.addEventListener("click", veranderen3);
-function veranderen3() { 
+
+function veranderen3() {
     deDerdeButton.classList.toggle("veranderen");
     document.body.classList.toggle("detailstonen");
 
@@ -39,10 +40,22 @@ function veranderen3() {
 
 var deVierdeButton = document.querySelector("ol li:nth-of-type(4) button");
 deVierdeButton.addEventListener("click", veranderen4);
-function veranderen4() { 
+
+function veranderen4() {
     deVierdeButton.classList.toggle("veranderen");
     document.body.classList.toggle("detailstonen");
 
     // achtergrond
     document.body.classList.toggle("snoopy4");
+}
+
+var deVijfdeButton = document.querySelector("ol li:nth-of-type(5) button");
+deVijfdeButton.addEventListener("click", veranderen5);
+
+function veranderen5() {
+    deVijfdeButton.classList.toggle("veranderen");
+    document.body.classList.toggle("detailstonen");
+
+    // achtergrond
+    document.body.classList.toggle("snoopy5");
 }
